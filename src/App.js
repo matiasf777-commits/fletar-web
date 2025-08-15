433ae51e08e5f82524ce4284eb0fc2b3c8161844
matiasf777-commits <matiasf777@gmail.com>
2025-08-15 12:53:47 -0300

    Update App.js
    Signed-off-by: matiasf777-commits <matiasf777@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import Footer from './components/Footer';
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import ClienteRegistro from './ClienteRegistro';
-import TransportistaRegistro from './TransportistaRegistro';
-import Login from './Login';
+import Footer from './components/Footer'; // Importa el Footer
 
-const Home = () => (
-  <div className="container">
-    <h1>FLETAR</h1>
-    <p>¿Qué sos?</p>
-    <div className="buttons">
-      <Link to="/cliente" className="btn">Soy Cliente</Link>
-      <Link to="/transportista" className="btn">Soy Transportista</Link>
-      <Link to="/login" className="btn outline">Ya tengo cuenta</Link>
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Bienvenido a Fletiq</h1>
+      </header>
+
+      <main>
+        <p>Contenido principal de la aplicación...</p>
+      </main>
+
+      <Footer /> {/* Footer con enlaces legales */}
     </div>
-  </div>
-);
-
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cliente" element={<ClienteRegistro />} />
-      <Route path="/transportista" element={<TransportistaRegistro />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </Router>
-);
+  );
+}
 
 export default App;
-<Footer />
 
